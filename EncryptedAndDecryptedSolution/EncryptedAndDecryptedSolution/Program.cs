8a1cfebf0874ad13302c8c0324b90ef01fbba453
keremcivelek <keremcivelek@hotmail.com.tr>
2023-10-18 17:38:15 +0300

    first commit

---
+++ EncryptedAndDecryptedSolution/EncryptedAndDecryptedSolution/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using EncryptionFeature;
+
+namespace EncryptedAndDecryptedSolution
+{
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            string jsonData = "{\"UserExist\":1,\"UserID\":1,\"Active\":1,\"UserName\":\"user1\",\"GroupId\":1,\"GroupType\":\"A\",\"GroupName\":\"Test_xXx\",\"WebMenuId\":0,\"LocationId\":0,\"Currency\":null,\"Credit\":0,\"logintoken\":\"0C1141EE3A0540428019278175038664\",\"PassChanged\":true,\"Country\":null}";
+
+            // Set Secret Key according to your wishes
+            string secretKey = Encryption.GenerateRandomKey();
+
+            // Generate a random IV
+            byte[] iv = Encryption.GenerateIV();
+
+            // Encrypt data
+            string encryptedData = Encryption.Encrypt(jsonData, secretKey, iv);
+            Console.WriteLine("Encrypted Data: " + encryptedData);
+
+            // Decrypt
+            string decryptedData = Encryption.Decrypt(encryptedData, secretKey, iv);
+            Console.WriteLine("Decoded Data: " + decryptedData);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EncryptedAndDecryptedSolution/EncryptionFeature/Encryption.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EncryptionFeature
+{
+    public static class Encryption
+    {
+        public static string Encrypt(string plainText, string secretKey, byte[] iv)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(secretKey);
+                aesAlg.IV = iv;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        public static string Decrypt(string cipherText, string secretKey, byte[] iv)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(secretKey);
+                aesAlg.IV = iv;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+        public static byte[] GenerateIV()
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.GenerateIV();
+                return aesAlg.IV;
+            }
+        }
+
+        public static string GenerateRandomKey(int keyLength = 32)
+        {
+            //32 characters required for 256 bits
+            const string validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=[]{}|;:,.<>?";
+
+            StringBuilder key = new StringBuilder(keyLength);
+            Random random = new Random();
+
+            for (int i = 0; i < keyLength; i++)
+            {
+                key.Append(validChars[random.Next(0, validChars.Length)]);
+            }
+
+            return key.ToString();
+        }
+    }
+}
